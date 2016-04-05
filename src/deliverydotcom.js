857213b9d22dd069a963d9da06594bbac1450ade
Faaez Ul Haq <faaez.ulhaq@gmail.com>
2016-04-05 02:06:41 -0400

    adding checkout

--- src/deliverydotcom.js
+++ src/deliverydotcom.js
@@ -5,12 +5,10 @@ var qs = require('querystring');
 var deliverydotcom = {
     searchDelivery: function(params,callback){
         var options = {url:config.API_URL+'merchant/search/delivery?'+qs.stringify(params),headers:{'Authorization':config.OAUTH_TOKEN}};
-        console.log(options.url);
         request(options, function(err,response,body){
             if(err) {
                 console.log(err);
             }
-            console.log(body);
             callback(JSON.parse(body));
         });
     }
